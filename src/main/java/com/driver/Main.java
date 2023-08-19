119f946af3512b0ca64403a5182f551ecb3c5e18
amank <aman.kaushik.975@gmail.com>
2023-08-19 12:14:45 +0530

    attempt2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,13 +3,14 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly ob = new RWOnly();
-        ob.name = "Aman"; // Error  : Cannot access private varialbe outside its class
-
-        System.out.println(ob.name); // Error  : Cannot access private varialbe outside its class
+//        ob.name = "Aman"; // Error  : Cannot access private varialbe outside its class
+//
+//        System.out.println(ob.name); // Error  : Cannot access private varialbe outside its class
 
         // after getter and setter
         ob.setName("Aman");
         System.out.println(ob.getName());
+        // finish
     }
   
 }
